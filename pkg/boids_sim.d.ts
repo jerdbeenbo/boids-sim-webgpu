452493558274882371146edf86b55a47935fa32f
Jarred Jenkins <jarredjenkins1000@gmail.com>
2025-05-27 01:45:26 +1000

    add /pkg

---
+++ pkg/boids_sim.d.ts
@@ -0,0 +1,47 @@
+/* tslint:disable */
+/* eslint-disable */
+export function setup(): void;
+/**
+ * The Flock struct is a collection of Vec<Boid> associated with a flockid
+ */
+export class Flock {
+  free(): void;
+  constructor(amt: number);
+  update(): void;
+  get_positions(): Float32Array;
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly __wbg_flock_free: (a: number, b: number) => void;
+  readonly flock_new: (a: number) => number;
+  readonly flock_update: (a: number) => void;
+  readonly flock_get_positions: (a: number) => [number, number];
+  readonly setup: () => void;
+  readonly __wbindgen_export_0: WebAssembly.Table;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ pkg/boids_sim_bg.wasm.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_flock_free: (a: number, b: number) => void;
+export const flock_new: (a: number) => number;
+export const flock_update: (a: number) => void;
+export const flock_get_positions: (a: number) => [number, number];
+export const setup: () => void;
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
